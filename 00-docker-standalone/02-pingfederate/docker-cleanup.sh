61071c2fefb6ca8572208ab25f7a6803b337d022
Glen Morgan <gmorgan@pingidentity.com>
2019-01-31 14:42:46 -0400

    formatting/consistency changes

--- 00-docker-standalone/02-pingfederate/docker-cleanup.sh
+++ 00-docker-standalone/02-pingfederate/docker-cleanup.sh
@@ -6,7 +6,7 @@ SHARED=$( cd ../FF-shared;pwd )
 # load the shared variables
 test -f "${SHARED}/env_vars" && source "${SHARED}/env_vars"
 
-# load the pingdirectory variables
+# load the pingfederate variables
 test -f "${HERE}/env_vars" && source "${HERE}/env_vars"
 
 if ! test -z "$(docker container ls -a --filter name=${CONTAINER_NAME} -q)" ; then 
@@ -14,7 +14,7 @@ if ! test -z "$(docker container ls -a --filter name=${CONTAINER_NAME} -q)" ; th
 fi
 
 for directory in "${RT_ROOT}/${CONTAINER_NAME}"; do 
-    echo "would you like to wipe the input directory ${directory} ? (y/n)"
+    echo "Would you like to remove the input directory ${directory} ? (y/n)"
     read answer
     answer=$( echo "${answer}" | tr [A-Z] [a-z] )
     case "${answer}" in
