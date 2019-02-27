20fb174e60dfd928c023c4691341f58a1187cdf9
Terry Sigle <terrysigle@pingidentity.com>
2019-02-27 09:15:59 -0700

    performed shellcheck

--- 10-docker-standalone/docker-cleanup.sh
+++ 10-docker-standalone/docker-cleanup.sh
@@ -2,10 +2,10 @@
 CMD="${0}"
 CONTAINER="${1}"
 FORCE="${2}"
-CONTAINER_DIR=$(cd $( dirname ${0} );pwd )
-SHARED_DIR=$( cd FF-shared;pwd )
+CONTAINER_DIR=$(cd $( dirname ${0} ) && pwd )
+SHARED_DIR=$( cd FF-shared && pwd )
 
-function usage()
+usage()
 {
 cat <<EO_USAGE
 
@@ -35,7 +35,7 @@ run_cmd() {
         INSTANCE=$1
 
         echo "Running ${CMD} ${INSTANCE} ${FORCE}..."
-        ${CMD} ${INSTANCE} ${FORCE}
+        ${CMD} "${INSTANCE}" "${FORCE}"
 }
 
 case ${CONTAINER} in
@@ -63,26 +63,28 @@ case ${CONTAINER} in
 esac
 
 # load the shared variables
-test -f "${SHARED_DIR}/env_vars" && source "${SHARED_DIR}/env_vars"
+#shellcheck source=/dev/null
+test -f "${SHARED_DIR}/env_vars" && . "${SHARED_DIR}/env_vars"
 
 # load the pingdirectory variables
-test -f "${CONTAINER_DIR}/env_vars" && source "${CONTAINER_DIR}/env_vars"
+#shellcheck source=/dev/null
+test -f "${CONTAINER_DIR}/env_vars" && . "${CONTAINER_DIR}/env_vars"
 
 if ! test -z "$(docker container ls -a --filter name=${CONTAINER_NAME} -q)" ; then 
-    docker container rm ${CONTAINER_NAME} ${FORCE}
+    docker container rm "${CONTAINER_NAME}" "${FORCE}"
 fi
 
 for directory in "${IN_DIR}" "${OUT_DIR}" "${RT_ROOT}/${CONTAINER_NAME}"; do 
-    if ! test -z ${directory}; then
-       if [ "${FORCE}" == "--force" ]; then
-          rm -rf ${directory}
+    if ! test -z "${directory}" ; then
+       if test "${FORCE}" = "--force" ; then
+          rm -rf "${directory}"
        else
          echo -n "Would you like to remove the input directory ${directory} (y/n) ? "
-         read answer
+         read -r answer
          answer=$( echo "${answer}" | tr [A-Z] [a-z] )
          case "${answer}" in
              y|yes)
-                 rm -rf ${directory}
+                 rm -rf "${directory}"
                  ;;
              *)
                  ;;
