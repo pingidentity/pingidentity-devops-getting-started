1b91e6a0f160db019bb07931d0a46b25f911b8e3
arno <arnaudlacour@pingidentity.com>
2019-02-28 19:02:31 -0700

    helper script to get the latest data.zip from a running ping access container and save it to server profile

---
+++ 99-helper-scripts/get-server-profile-from-pingaccess-container.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+if test -z "${1}" ; then
+    echo the first argument must be a container id
+    exit 1
+fi
+
+if test -z "${2}" ; then
+    echo "The second argument must the path to the server profile"
+    exit 2
+fi
+
+if ! test -d "${2}" ; then
+  echo the path to the server profile does not exist
+  exit 3
+fi
+
+if ! test $(basename "${2}") = "instance" ; then
+    echo "the server profile path should end with 'instance' (i.e. </path/to/pa-server-profile>/instance)"
+    exit 4
+fi
+containerId="${1}"
+serverPofilePath="${2}"
+
+# check the container is live
+if docker container ls --format '{{.ID}}' --filter id=${containerId} |grep "${containerId}" >/dev/null ; then 
+    docker cp ${containerId}:/opt/out/instance/data/archive/latest.data.zip /tmp
+    unzip -d "${serverPofilePath}" /tmp/latest.data.zip
+else
+    echo the provided container ID ${containerId} does not appear to be running locally
+    exit 5cd 
+fi
