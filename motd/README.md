d959186b5269399ae1d55a2f38ef21c805fd598f
Terry Sigle <terrysigle@pingidentity.com>
2019-08-03 15:30:53 -0500

    MOTD README and testing script to test proposed motd changes

---
+++ motd/README.md
@@ -0,0 +1,63 @@
+# Ping Identity DevOps Message of the Day (MOTD)
+
+A message of the day json can be used to provide a motd file to Ping Idnetity DevOps Images when they start.
+
+The name and location of the file should be in the Github `pingidentity-devops-getting-started` repo in the
+`motd/motd.json` file.
+
+Additionally, if there is a `motd` file in the top level of the server-profile, this will simply 
+be appended to the `/etc/moted` file of the image.
+
+Upon changing this file, the author can test the new messages using the `test-motd.sh` script.
+
+## test-mod.sh script
+
+To test the mod.json on github, simply use:
+
+    ./test-motd.sh github
+
+To test the mod.json locally, simply use:
+
+    ./test-motd.sh local
+
+The script will provide the `JQ_EXPR` used that should always match that in the
+`09-build-motd.sh` hook.
+
+## Example motd.json
+
+The example below is an example of messages that will be shown for all types of 
+images (i.e. devops) and pingfederate images.  Note that the messsages will only
+be shown starting on the `validFrom` date to the `validTo` date.
+
+```
+{
+    "devops" : [
+        {
+            "validFrom": 20190701,
+            "validTo": 20190730,
+            "subject": "General Message 1",
+            "message": ["This is line # 1",
+                        "",
+                        "This is line # 3",]
+        },     
+        {
+            "validFrom": 20190801,
+            "validTo": 20190830,
+            "subject": "Genearl Message 2",
+            "message": ["Message goes here"]
+        }
+    ],
+    "pingfederate" : [
+        {
+            "validFrom": 20190701,
+            "validTo": 20190830,
+            "subject": "PingFederate Message 1",
+            "message": ["Message goes here"]
+        }
+    ]
+}
+```
+
+## Copyright
+
+Copyright © 2019 Ping Identity. All rights reserved.
---
+++ motd/test-motd.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+cd "$( dirname "${0}" )"
+THIS="$( basename "${0}" )"
+THIS_DIR=`pwd`
+
+CURR_DATE=$(date +%Y%m%d)
+
+#
+# Usage printing function
+#
+usage ()
+{
+cat <<END_USAGE
+Usage: $0 {github|local}
+    where {options} include:
+
+    github
+        Test the motd.json file from github
+    local
+        Test the motd.json file from local directory
+
+    One of these options are required
+
+END_USAGE
+exit 99
+}
+
+while ! test -z "${1}" ; do
+    case "${1}" in
+        github)
+            shift
+
+            MOTD_FILE="/tmp/motd.json.$$"
+            MOTD_URL="https://raw.githubusercontent.com/pingidentity/pingidentity-devops-getting-started/master/motd/motd.json"
+
+            motdCurlResult=$(curl -G -o "${MOTD_FILE}" -w '%{http_code}' \
+                "${MOTD_URL}" \
+                2> /dev/null)
+
+            if test $motdCurlResult -eq 200 ; then
+                echo "Successfully downloaded motd.json from Github"
+            else
+                echo "ERROR: Unable to download motd.json from Github "
+                echo "  URL: ${MOTD_URL}"
+                exit 1
+            fi
+            MOTD_SOURCE="gitlab"
+            ;;
+
+        local)
+            shift
+
+            MOTD_FILE="${THIS_DIR}/motd.json"
+            MOTD_SOURCE="local"
+            ;;
+        *)
+            echo "Unrecognized option"
+            usage
+            ;;
+    esac
+    shift
+done
+
+test -z ${MOTD_SOURCE} && usage
+
+
+JQ_EXPR=".[] | select(.validFrom <= ${CURR_DATE} and .validTo >= ${CURR_DATE}) |
+               \"\n---- SUBJECT: \" + .subject + \"\n\" +
+                         (.message | join(\"\n\")) +
+               \"\n\""
+
+echo "
+################################################################################
+#                         Testing MOTD
+#         SOURCE = ${MOTD_SOURCE}
+#      MOTD_FILE = ${MOTD_FILE}
+#   CURRENT_DATE = ${CURR_DATE}
+#        JQ_EXPR = ${JQ_EXPR}
+################################################################################"
+
+JQ_EXPR=".[] | select(.validFrom <= ${CURR_DATE} and .validTo >= ${CURR_DATE}) |
+               \"\n---- SUBJECT: \" + .subject + \"\n\" +
+                         (.message | join(\"\n\")) +
+               \"\n\""
+
+for category in "devops" "pingaccess" "pingdatagovernance" "pingdatasync" "pingdirectory" "pingdirectoryproxy" "pingfederate" ;
+do
+    echo "
+################################################################################
+#                         Testing '${category}' Category
+################################################################################"
+
+    cat ${MOTD_FILE} | \
+            jq -r ".${category} | $JQ_EXPR"
+done
