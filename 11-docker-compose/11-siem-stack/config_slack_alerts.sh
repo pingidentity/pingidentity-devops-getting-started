46b27fbe18465e60f5b1870cb6ee7b01c687c0db
ryanivis <ryanivis@pingidentity.com>
2020-03-12 07:50:28 -0600

    fixing slack alerting bug

--- 11-docker-compose/11-siem-stack/config_slack_alerts.sh
+++ 11-docker-compose/11-siem-stack/config_slack_alerts.sh
@@ -13,3 +13,24 @@ if [[ $SLACK_WEB_HOOK != "" ]]; then
 	docker exec es01 /bin/sh -c ". /usr/share/elasticsearch/bootstrap/keystore_bootstrap.sh $SLACK_WEB_HOOK"
 	docker exec es02 /bin/sh -c ". /usr/share/elasticsearch/bootstrap/keystore_bootstrap.sh $SLACK_WEB_HOOK"
 fi
+
+echo "------------------------------------------------------------------------------ "
+echo "Please Enter Your 'elastic' user password, this is used to load watchers...  "
+echo ">> ---- Enter Now ----"
+read -s ELASTIC_PASSWORD
+
+if [[ $ELASTIC_PASSWORD != "" ]]; then
+	echo "Load in ElasticSearch Watchers"
+	sleep 5
+	#Loading Saved Watchers
+	for f in ./elasticsearch-siem/watchers/*.json
+	do	
+	  echo "Processing watcher file (full path) $f "
+	  echo "start"
+	  fn=$(basename $f)
+	  n="${fn%.*}"
+
+	  echo "Processing file name $n "
+	  curl -X PUT "https://localhost:9201/_watcher/watch/$n?pretty" --insecure -u elastic:$ELASTIC_PASSWORD  -H 'Content-Type: application/json' -d"@$f"
+	done
+fi
--- 11-docker-compose/11-siem-stack/elasticsearch-siem/ping_siem_config_bootstrap/saved_state_loader.sh
+++ 11-docker-compose/11-siem-stack/elasticsearch-siem/ping_siem_config_bootstrap/saved_state_loader.sh
@@ -13,6 +13,7 @@ do
   health=$(curl -s -u elastic:$ELASTIC_PASSWORD --insecure https://es01:9200/_cluster/health)
   es_status=$(expr "$health" : '.*"status":"\([^"]*\)"')
 done
+
 echo $health
 echo "Loading! -- ElasticSearch Index Templates"
 curl -X PUT "https://es01:9200/_template/pf_audit?pretty" --insecure -u elastic:$ELASTIC_PASSWORD -H 'Content-Type: application/json' -d"@/usr/share/elasticsearch/config/es_config/pf_template.json"
@@ -20,35 +21,18 @@ curl -X PUT "https://es01:9200/_template/pd_access?pretty" --insecure -u elastic
 curl -X PUT "https://es01:9200/_template/pa_audit?pretty" --insecure -u elastic:$ELASTIC_PASSWORD -H 'Content-Type: application/json' -d"@/usr/share/elasticsearch/config/es_config/pa_template.json"
 
 #Wait for Kibana API to go Green before importing saved objects
-echo "loading kibana saved objects"
+echo "Waiting for Kibana status green, prior to loading saved objects..."
 while [ "$kib_status" != "Looking good" ]
 do
-	echo "Status Not Looking good Yet Kibana"
+	echo "Status Not Looking green Yet Kibana"
 	sleep 3
 	health=$(curl -s -u elastic:$ELASTIC_PASSWORD --insecure https://kibana:5601/api/status)
 	echo $health
 	kib_status=$(expr "$health" : '.*"nickname":"\([^"]*\)"')
 done
 
-echo $health
 echo "Loading! -- Kibana Saved Objects."
 curl -X POST "https://kibana:5601/api/saved_objects/_import" --insecure -u elastic:$ELASTIC_PASSWORD -H "kbn-xsrf: true" --form file="@/usr/share/elasticsearch/config/kibana_config/kib_base.ndjson"
-
-echo "Load in Watchers"
-sleep 5
-#Loading Saved Watchers
-for f in /usr/share/elasticsearch/watchers/*.json
-do	
-  echo "Processing watcher file (full path) $f "
-  echo "start"
-  fn=$(basename $f)
-  n="${fn%.*}"
-  echo "$n"
-  echo "end"
-
-  echo "Processing file name $n "
-  curl -X PUT "https://es01:9200/_watcher/watch/$n?pretty" --insecure -u elastic:$ELASTIC_PASSWORD  -H 'Content-Type: application/json' -d"@$f"
-done
 
 echo "bootstrap execution complete."
 
